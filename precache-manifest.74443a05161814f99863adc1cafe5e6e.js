3fad4efff90021d11902747d053dbc29c2ab1e55
jinbot <jhjinbhang@gmail.com>
2019-11-19 15:34:27 +0900

    Updates

---
+++ precache-manifest.74443a05161814f99863adc1cafe5e6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01faab2df24327e2b8fea0a5babc497",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "d71482483e49442bfa1e",
+    "url": "/movie_app_2019/static/css/main.44f21bac.chunk.css"
+  },
+  {
+    "revision": "4e3d0de9869208895014",
+    "url": "/movie_app_2019/static/js/2.5b209c19.chunk.js"
+  },
+  {
+    "revision": "d71482483e49442bfa1e",
+    "url": "/movie_app_2019/static/js/main.f08430c3.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
